283353ffd32e8b3ec9b8f7191ec78a9ed92b170c
nmifzal <nmifzal@gmail.com>
2023-03-16 22:19:12 +0530

    first commit

---
+++ src/api/test-table/controllers/test-table.ts
@@ -0,0 +1,7 @@
+/**
+ * test-table controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::test-table.test-table');
---
+++ src/api/test-table/routes/test-table.ts
@@ -0,0 +1,7 @@
+/**
+ * test-table router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::test-table.test-table');
